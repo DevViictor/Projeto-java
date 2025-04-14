d17d6d6521861c7a30e94994cdc99bf6d27e4ece
ProgamerPY <victorritossantos@gmail.com>
2025-04-13 21:17:31 -0300

    first commit

---
+++ teste.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+
+
+public class teste{
+    public static void main(String[] args) {
+        
+        cadastro c1 = new cadastro();
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        c1.nome = sc.nextLine();
+
+        
+
+    }
+}
